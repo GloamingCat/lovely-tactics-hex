5467441c604f42369e9e84fe01813675e2e0d669
GloamingCat <nightlywhiskers@gmail.com>
2018-03-14 08:59:53 -0300

    MemberInfoWindow page counting

--- scripts/core/gui/members/window/MemberInfoWindow.lua
+++ scripts/core/gui/members/window/MemberInfoWindow.lua
@@ -9,6 +9,7 @@ A window that shows HP and MP of a troop member.
 
 -- Imports
 local MemberInfo = require('core/gui/general/widget/MemberInfo')
+local SimpleText = require('core/gui/widget/SimpleText')
 local Vector = require('core/math/Vector')
 local Window = require('core/gui/Window')
 
@@ -18,11 +19,15 @@ local MemberInfoWindow = class(Window)
 -- Initialization
 ---------------------------------------------------------------------------------------------------
 
+-- Constructor.
+-- @param(member : Battler) The initial member.
+-- @param(...) Other default parameters from Window:init.
 function MemberInfoWindow:init(member, ...)
   self.member = member
   Window.init(self, ...)
 end
-
+-- Overrides Window:createContent.
+-- Creates the content of the initial member.
 function MemberInfoWindow:createContent(...)
   Window.createContent(self, ...)
   self:setMember(self.member)
@@ -32,6 +37,7 @@ end
 -- Member
 ---------------------------------------------------------------------------------------------------
 
+-- @param(member : Battler) Changes the member info to another member's.
 function MemberInfoWindow:setMember(member)
   self.member = member
   if self.info then
@@ -46,6 +52,24 @@ function MemberInfoWindow:setMember(member)
   if not self.open then
     self.info:hide()
   end
+end
+-- Sets the paging.
+-- @param(current : number) Current page.
+-- @param(max : number) Total number of pages.
+function MemberInfoWindow:setPage(current, max)
+  if not self.page then
+    local font = Fonts.gui_tiny
+    local x = -self.width / 2 + self:hPadding()
+    local y = self.height / 2 - self:vPadding() - 6
+    self.page = SimpleText('', Vector(x, y, -1), self.width - self:hPadding(), 'left', font)
+    self.content:add(self.page)
+  end
+  local text = ''
+  if current then
+    text = max and (current .. '/' .. max) or (current .. '')
+  end
+  self.page:setText(text)
+  self.page:redraw()
 end
 
 ---------------------------------------------------------------------------------------------------
