78da902aa7cf31d81457d2669af041bcdda9f514
GloamingCat <nightlywhiskers@gmail.com>
2019-07-30 16:15:20 -0300

    english vocab

---
+++ scripts/vocab/EnglishDialogues.lua
@@ -0,0 +1,16 @@
+
+return {
+  
+  -- Dialogue Test
+  Hi = "Hi.",
+  WhatsYourAge = "What's your age?",
+  OhMeToo = "Oh, me too.",
+  HowAreYouDoing = "How you {i}doing{r}? ~",
+  Good = "Good.",
+  Bad = "Bad.",
+  ThatsGood = "That's good.",
+  ThatsBad = "That's bad.",
+  ImHungry = "I'm hungry. Maybe I'll have some pudding."
+  
+  -- 
+}
