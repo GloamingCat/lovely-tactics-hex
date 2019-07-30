cd5c76bc1022ce6798df209bd1f8cffe804c681b
GloamingCat <nightlywhiskers@gmail.com>
2019-07-30 11:51:19 -0300

    Inn scene

--- scripts/custom/events/town/Inn.lua
+++ scripts/custom/events/town/Inn.lua
@@ -8,6 +8,51 @@ Inn
 
 return function(script)
 
+  script:openDialogueWindow { id = 1, x = 0, y = -ScreenManager.height / 3,
+    width = ScreenManager.width / 3,
+    height = ScreenManager.height / 4
+  }
+  
+  script:showDialogue { id = 1, character = '', message = 
+    "Descansar na casa de Dona Chita?"
+  }
 
+  script:openChoiceWindow { width = 80, choices = {
+    "Sim.",
+    "Não."
+  }}
+
+  script:closeDialogueWindow { id = 1 }
+
+  if script.gui.choice == 2 then
+    return
+  end
+  
+  script:fadeout { time = 60, wait = true }
+  AudioManager:playSFX { name = "sfx/GameAudio/heal.wav", pitch = 100, volume = 100 }
+  script:wait(90)
+  script:healAll {}
+  FieldManager.renderer.images.Inn:setVisible(true)
+  
+  script:fadein { time = 60, wait = true }
+
+  script:openDialogueWindow { id = 1, x = 0, y = ScreenManager.height / 3,
+    width = ScreenManager.width, 
+    height = ScreenManager.height / 3 
+  }
+
+  script:showDialogue { id = 1, portrait = 124, nameX = -0.45, nameY = -1.25, message = 
+    "Sempre que precisar, você pode voltar aqui para descansar ou para comprar mais comida e utilidades. Tenha cuidado."
+  }
+  
+  script:showDialogue { id = 1, character = "player", portrait = "Happy", nameX = -0.45, nameY = -1.25, message = 
+    "Pode deixar, Dona Chita!"
+  }
+  
+  script:closeDialogueWindow { id = 1 }
+
+  script:fadeout { time = 90, wait = true }
+  FieldManager.renderer.images.Inn:setVisible(false)
+  script:fadein { time = 90, wait = true }
 
 end
