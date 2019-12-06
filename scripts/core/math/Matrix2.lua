2284c9d04318a673386afc0ca25d7193a6356b2f
GloamingCat <nightlywhiskers@gmail.com>
2019-12-05 22:36:41 -0300

    Matrix2:positionOf

--- scripts/core/math/Matrix2.lua
+++ scripts/core/math/Matrix2.lua
@@ -14,8 +14,8 @@ local Matrix2 = class()
 ---------------------------------------------------------------------------------------------------
 
 -- Constructor.
--- @param(width : number) The number of lines.
--- @param(height : number) The number of columns.
+-- @param(width : number) The number of columns.
+-- @param(height : number) The number of lines.
 -- @param(startValue : unknown) The initial value of every element (optional).
 function Matrix2:init(width, height, startValue)
   self.width = width
@@ -42,18 +42,18 @@ end
 ---------------------------------------------------------------------------------------------------
 
 -- Gets the element at line i and column j.
--- @param(i : number) line
--- @param(j : number) column
--- @ret(unknown) the current value at that position
+-- @param(i : number) Column number.
+-- @param(j : number) Line number.
+-- @ret(unknown) The current value at that position.
 function Matrix2:get(i, j)
   if i >= 1 and i <= self.width and j >= 1 and j <= self.height then
     return self[(j - 1) * self.width + i]
   end
 end
 -- Sets the element at line i and column j.
--- @param(value : unknown) the new value at that position
--- @param(i : number) line
--- @param(j : number) column
+-- @param(value : unknown) The new value at that position.
+-- @param(i : number) Column number.
+-- @param(j : number) Line number.
 function Matrix2:set(value, i, j)
   if i >= 1 and i <= self.width and j >= 1 and j <= self.height then
     self[(j - 1) * self.width + i] = value
@@ -74,11 +74,11 @@ function Matrix2:iterator()
 end
 
 ---------------------------------------------------------------------------------------------------
--- Operations
+-- Numeric Operations
 ---------------------------------------------------------------------------------------------------
 
 -- Matrixes multiplication.
--- @param(other : Matrix2 | table) the right operand
+-- @param(other : Matrix2 | table) The right operand.
 function Matrix2:__mul(other)
   if other.height and other.width then
     return self:mulMatrix(other)
@@ -87,7 +87,7 @@ function Matrix2:__mul(other)
   end
 end
 -- Matrix multiplication.
--- @param(other : Matrix2) the right matrix
+-- @param(other : Matrix2) The right operand.
 function Matrix2:mulMatrix(other)
   assert(self.height == other.width, 'Cannot multiply matrixes: ' .. self.height .. ' ' .. other.width)
   local m = Matrix2(self.width, other.height, 0)
@@ -102,8 +102,8 @@ function Matrix2:mulMatrix(other)
   end
   return m
 end
--- Matrix x vector multiplication
--- @param(vector : table) the right vector (numeric array)
+-- Matrix x vector multiplication.
+-- @param(vector : table) The right vector (numeric array).
 function Matrix2:mulVector(vector)
   assert(self.height == #vector, 'Cannot multiply with vector: ' .. self.height .. ' ' .. #vector)
   local m = {}
@@ -115,6 +115,23 @@ function Matrix2:mulVector(vector)
     m[i] = value
   end
   return m
+end
+
+---------------------------------------------------------------------------------------------------
+-- General
+---------------------------------------------------------------------------------------------------
+
+-- Finds the coordinates of given element.
+-- @ret(number) Column number (nil if not found).
+-- @ret(number) Line number (nil if not found).
+function Matrix2:positionOf(element)
+  local size = self.width * self.height
+  for i = 1, size do
+    if self[i] == element then
+      i = i - 1
+      return i % self.width + 1, math.floor(i / self.width) + 1
+    end
+  end
 end
 -- @ret(string) The string representation (for debugging).
 function Matrix2:__tostring()
