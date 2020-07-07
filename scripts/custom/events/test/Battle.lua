9090e9d5a2e7d2f50cb82aaadc4904eeccb0519e
GloamingCat <nightlywhiskers@gmail.com>
2020-07-07 02:39:36 -0300

    enemy NPC cooldown

--- scripts/custom/events/test/Battle.lua
+++ scripts/custom/events/test/Battle.lua
@@ -9,6 +9,14 @@ Starts a battle when this collides with player.
 
 return function(script)
 
+  coroutine.yield()
+
+  if script.char.cooldown and script.char.cooldown > 0 then
+    -- After-battle cooldown (escape or lose)
+    script.char.cooldown = script.char.cooldown - GameManager:frameTime() * 60
+    --return
+  end
+
   if script.collider ~= script.player and script.collided ~= script.player then
     -- Collided with something else
     return
@@ -20,20 +28,29 @@ return function(script)
   end
   
   script.player:playIdleAnimation()
-  script:startBattle { fieldID = script.args.fieldID or 0, fade = 60, intro = true, 
-    gameOverCondition = 1, escapeEnabled = true }
+  script:startBattle { 
+    fieldID = tonumber(script.args.fieldID) or 0, 
+    fade = 60, 
+    intro = true, 
+    gameOverCondition = script.args.loseEnabled == 'true' and 0 or 1, 
+    escapeEnabled = true 
+  }
   
   if BattleManager:playerWon() then
     print 'You won!'
     script:deleteChar { key = "self", fade = 60, permanent = true }
   elseif BattleManager:enemyWon() then
     print 'You lost...'
+    script.char.cooldown = 120
   elseif BattleManager:drawed() then
     print 'Draw.'
+    script.char.cooldown = 120
   elseif BattleManager:playerEscaped() then
     print 'You escaped!'
+    script.char.cooldown = 120
   elseif BattleManager:enemyEscaped() then
     print 'The enemy escaped...'
+    script:deleteChar { key = "self", fade = 60, permanent = true }
   end
 
 end
